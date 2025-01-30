8981c87b73180dc6ec966c3f10789bf531a859a0
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-01-29 21:39:09 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,28 @@
 package application;
+
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        limparTela();
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos);
+    }
+
+    public static void limparTela() {
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (final Exception e) {
+            // Trata exceções (pode ser uma exceção de interrupção)
+            e.printStackTrace();
+        }
     }
 }
