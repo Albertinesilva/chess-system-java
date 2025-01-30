d67a1c4c3cb58824b7ee89337734725a011602bb
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-01-29 21:55:07 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,12 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
     public static void main(String[] args) throws Exception {
         limparTela();
-        Position pos = new Position(3, 5);
-
-        System.out.println(pos);
+        
+        Board board = new Board(8,8);
     }
 
     public static void limparTela() {
